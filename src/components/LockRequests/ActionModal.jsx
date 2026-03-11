e2782ab0f36fcf7124c913512079c33e55d0a974
Rafiq Shaik <rafiqshaik097@gmail.com>
2026-03-11 18:12:42 +0530

    changes in options

--- src/components/LockRequests/ActionModal.jsx
+++ src/components/LockRequests/ActionModal.jsx
@@ -4,16 +4,21 @@ import { CloseIcon, ChevronDownIcon } from '../../icons/svgIcons';
 const rejectOptions = [
   { value: "Not Permitted", label: "Not Permitted" },
   { value: "You can't access", label: "You can't access" },
-  { value: "Request again", label: "Request again" },
+  { value: "Please Request again", label: "Please Request again" },
   { value: "Others", label: "Others (Specify below)" },
 ];
 
 const durationOptions = [
   { value: "5", label: "5 minutes" },
   { value: "10", label: "10 minutes" },
+  { value: "15", label: "15 minutes" },
+  { value: "20", label: "20 minutes" },
   { value: "30", label: "30 minutes" },
+  { value: "45", label: "45 minutes" },
   { value: "60", label: "1 hour" },
+  { value: "90", label: "90 minutes" },
   { value: "120", label: "2 hours" },
+  { value: "180", label: "3 hours" },
 ];
 
 const CustomSelect = ({ value, onChange, options, placeholder }) => {
